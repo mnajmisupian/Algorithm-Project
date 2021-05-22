a0c1f7444a0f3a0e9b6df1e4d016ebe0d10f301b
Najmi Supian <mnajmisupian@gmail.com>
2021-05-23 00:19:29 +0800

    citylink

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Problem2 import problem2 as P2
+from Problem2 import problem2citylink as CityLink
 
-P2.jnt("https://www.straitstimes.com/asia/se-asia/pandemic-fuelled-e-shopping-boom-spurs-courier-firms-growth",
-"https://www.panaynews.net/jt-express-celebrates-another-milestone-with-foton/",
-"https://kr-asia.com/one-masters-two-apprentices-how-indonesias-jt-express-rose-in-china-on-the-back-of-pinduoduo")
+# P2.jnt("https://www.straitstimes.com/asia/se-asia/pandemic-fuelled-e-shopping-boom-spurs-courier-firms-growth",
+# "https://www.panaynews.net/jt-express-celebrates-another-milestone-with-foton/",
+# "https://kr-asia.com/one-masters-two-apprentices-how-indonesias-jt-express-rose-in-china-on-the-back-of-pinduoduo")
 
+CityLink.citylink("http://autoworld.com.my/news/2020/09/25/city-link-express-takes-delivery-of-277-new-isuzu-trucks/",
+"https://www.thestar.com.my/business/business-news/2015/01/05/citylink-mulls-main-market-listing-in-three-years",
+"https://www.thesundaily.my/gear-up/isuzu-lorries--city-link-s-preferred-choice-AK729310") 
