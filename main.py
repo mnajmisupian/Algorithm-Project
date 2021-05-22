dc7b070b0261dccc8554e2dc267814e2ff3b31d4
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2021-05-22 21:57:05 +0800

    Latest

---
+++ main.py
@@ -0,0 +1,6 @@
+from Problem2 import problem2 as P2
+
+P2.jnt("https://www.straitstimes.com/asia/se-asia/pandemic-fuelled-e-shopping-boom-spurs-courier-firms-growth",
+"https://www.panaynews.net/jt-express-celebrates-another-milestone-with-foton/",
+"https://kr-asia.com/one-masters-two-apprentices-how-indonesias-jt-express-rose-in-china-on-the-back-of-pinduoduo")
+
