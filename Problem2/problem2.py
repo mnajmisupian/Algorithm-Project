3bde4c27e84c4e4871ea69f29aeb7e4fab1a2765
IzzatHaikal <58548850+IzzatHaikal@users.noreply.github.com>
2021-05-24 12:38:06 +0800

    Merge CL and PL into single file

--- Problem2/problem2.py
+++ Problem2/problem2.py
@@ -43,7 +43,7 @@ def jnt(first, second, third):
     # print(wordValueList)
     # print(wordKeyList)
 
-    barChart(wordKeyList, wordValueList, "Word Frequency", "Frequency", "Word", 2, "Problem2\J&T\wordCount", 50, 50)
+    barChart(wordKeyList, wordValueList, "Word Frequency", "Frequency", "Word", 8, "Problem2\J&T\wordCount", 150, 150)
 
     posWordList = posWords()
 
@@ -54,24 +54,126 @@ def jnt(first, second, third):
     negativeWord,negativeWordVal,negative = negWordsNVal(negWordList, wordDic)
 
     barChart(positiveWord, positiveWordVal, "Positive Word Frequency", "Frequency", "Positive Word", 10, "Problem2\J&T\posWord", 20, 20)
-    barChart(negativeWord, negativeWordVal, "Negative Word Frequency", "Frequency", "Negative Word", 10, "Problem2\J&T\\negWord", 20, 20)
+    barChart(negativeWord, negativeWordVal, "Negative Word Frequency", "Frequency", "Negative Word", 20, "Problem2\J&T\\negWord", 20, 20)
     posVSnegBarChart(positive, negative, "Problem2\J&T\posNeg")
+
+def cityLink(first, second, third):
+    url0 = Request(first, headers={"User-Agent" : "Mozilla/5.0"})                           #scraping
+    pageOpen0 = urlopen(url0)
+    pageHtml0 = pageOpen0.read().decode("utf-8")
+    htmlParsed0 = BeautifulSoup(pageHtml0,"html.parser")
+    mainParsed0 = htmlParsed0.find("div", attrs={"class" : "td-post-text-content"})
+    mainP0 = mainParsed0.find_all("p")
+
+    url1 = Request(second, headers={"User-Agent" : "Mozilla/5.0"})
+    pageOpen1 = urlopen(url1)
+    pageHtml1 = pageOpen1.read().decode("utf-8")
+    htmlParsed1 = BeautifulSoup(pageHtml1,"html.parser")
+    mainParsed1 = htmlParsed1.find("div", attrs={"id" : "story-body" , "class" : "story bot-15 relative"})
+    mainP1 = mainParsed1.find_all("p")
+
+    url2 = Request(third, headers={"User-Agent" : "Mozilla/5.0"})
+    pageOpen2 = urlopen(url2)
+    pageHtml2 = pageOpen2.read().decode("utf-8")
+    htmlParsed2 = BeautifulSoup(pageHtml2,"html.parser")
+    mainParsed2 = htmlParsed2.find("div", attrs={"class" : "paragraph" , "mlnid" : "idcon=270416;order=3.0"})
+    mainP2 = mainParsed2.find_all("p")
+
+    pList = pToList(mainP0, mainP1, mainP2)
+
+    stopWordList = stopWords()
+
+    wordList = paraToWords(pList, stopWordList)
+
+    wordDic = wordListToDic(wordList)
+
+    wordKeyList = list(wordDic.keys())                                                      #transfer word in the disctionary to list
+    wordValueList = []                                                                      #transfer word values(frequency) to list
+    for i in wordDic.values():                      
+        wordValueList.append(int(i))
+    # print(wordValueList)
+    # print(wordKeyList)
+
+    barChart(wordKeyList, wordValueList, "Word Frequency", "Frequency", "Word", 8, "Problem2\CityLink\wordCount", 100, 100)
+
+    posWordList = posWords()
+
+    negWordList = negWords()
+
+    positiveWord,positiveWordVal,positive = posWordsNVal(posWordList, wordDic) 
+
+    negativeWord,negativeWordVal,negative = negWordsNVal(negWordList, wordDic)
+
+    barChart(positiveWord, positiveWordVal, "Positive Word Frequency", "Frequency", "Positive Word", 10, "Problem2\CityLink\posWord", 20, 20)
+    barChart(negativeWord, negativeWordVal, "Negative Word Frequency", "Frequency", "Negative Word", 20, "Problem2\CityLink\\negWord", 20, 20)
+    posVSnegBarChart(positive, negative, "Problem2\CityLink\posNeg")
+
+def posLaju(first, second, third):
+    url0 = Request(first, headers={"User-Agent" : "Mozilla/5.0"})                           #scraping
+    pageOpen0 = urlopen(url0)
+    pageHtml0 = pageOpen0.read().decode("utf-8")
+    htmlParsed0 = BeautifulSoup(pageHtml0,"html.parser")
+    mainParsed0 = htmlParsed0.find("div", attrs={"id" : "story-body" , "class" : "story bot-15 relative"})
+    mainP0 = mainParsed0.find_all("p")
+
+    url1 = Request(second, headers={"User-Agent" : "Mozilla/5.0"})
+    pageOpen1 = urlopen(url1)
+    pageHtml1 = pageOpen1.read().decode("utf-8")
+    htmlParsed1 = BeautifulSoup(pageHtml1,"html.parser")
+    mainParsed1 = htmlParsed1.find("div", attrs={"class" : "entry-content"})
+    mainP1 = mainParsed1.find_all("p")
+
+    url2 = Request(third, headers={"User-Agent" : "Mozilla/5.0"})
+    pageOpen2 = urlopen(url2)
+    pageHtml2 = pageOpen2.read().decode("utf-8")
+    htmlParsed2 = BeautifulSoup(pageHtml2,"html.parser")
+    mainParsed2 = htmlParsed2.find("div", attrs={"class" : "post-content description"})
+    mainP2 = mainParsed2.find_all("p")
+
+    pList = pToList(mainP0, mainP1, mainP2)
+
+    stopWordList = stopWords()
+
+    wordList = paraToWords(pList, stopWordList)
+
+    wordDic = wordListToDic(wordList)
+
+    wordKeyList = list(wordDic.keys())                                                      #transfer word in the disctionary to list
+    wordValueList = []                                                                      #transfer word values(frequency) to list
+    for i in wordDic.values():                      
+        wordValueList.append(int(i))
+    # print(wordValueList)
+    # print(wordKeyList)
+
+    barChart(wordKeyList, wordValueList, "Word Frequency", "Frequency", "Word", 8, "Problem2\PosLaju\wordCount", 100, 100)
+
+    posWordList = posWords()
+
+    negWordList = negWords()
+
+    positiveWord,positiveWordVal,positive = posWordsNVal(posWordList, wordDic) 
+
+    negativeWord,negativeWordVal,negative = negWordsNVal(negWordList, wordDic)
+
+    barChart(positiveWord, positiveWordVal, "Positive Word Frequency", "Frequency", "Positive Word", 10, "Problem2\PosLaju\posWord", 20, 20)
+    barChart(negativeWord, negativeWordVal, "Negative Word Frequency", "Frequency", "Negative Word", 20, "Problem2\PosLaju\\negWord", 20, 20)
+    posVSnegBarChart(positive, negative, "Problem2\PosLaju\posNeg")
 
 def pToList(mainP0, mainP1, mainP2):
     pList = []                                                                              #change all <p> tag to string and replace all unwanted characters
     for i in mainP0:                                        
         holdText = i.text
-        holdText = re.sub("[-,.\"():]", "", holdText)
+        holdText = re.sub("[-,.\"():;“”‘’'—’?]", "", holdText)
         pList.append(holdText.lower())
     # print(pList[0])
     for i in mainP1:
         holdText = i.text
-        holdText = re.sub("[-,.\"():]", "", holdText)
+        holdText = re.sub("[-,.\"():;“”‘’'—’?]", "", holdText)
         pList.append(holdText.lower())
     # print(pList[0])
     for i in mainP2:
         holdText = i.text
-        holdText = re.sub("[-,.\"():]", "", holdText)
+        holdText = re.sub("[-,.\"():;“”‘’'—’?]", "", holdText)
         pList.append(holdText.lower())
     # print(pList[0])
     return pList
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ P2.jnt("https://www.straitstimes.com/asia/se-asia/pandemic-fuelled-e-shopping-bo
 "https://www.theedgemarkets.com/article/indonesias-jt-express-said-weigh-us1-billionplus-us-ipo",
 "https://kr-asia.com/one-masters-two-apprentices-how-indonesias-jt-express-rose-in-china-on-the-back-of-pinduoduo")
 
-# CityLink.citylink("http://autoworld.com.my/news/2020/09/25/city-link-express-takes-delivery-of-277-new-isuzu-trucks/",
-# "https://www.thestar.com.my/business/business-news/2015/01/05/citylink-mulls-main-market-listing-in-three-years",
-# "https://www.thesundaily.my/gear-up/isuzu-lorries--city-link-s-preferred-choice-AK729310") 
+P2.cityLink("http://autoworld.com.my/news/2020/09/25/city-link-express-takes-delivery-of-277-new-isuzu-trucks/",
+"https://www.thestar.com.my/business/business-news/2015/01/05/citylink-mulls-main-market-listing-in-three-years",
+"https://www.thesundaily.my/gear-up/isuzu-lorries--city-link-s-preferred-choice-AK729310") 
 
-# PosLaju.poslaju("https://www.thestar.com.my/business/business-news/2021/02/22/pos-malaysia-records-rm233b-revenue-in-fy20",
-# "https://soyacincau.com/2020/08/15/pos-malaysia-e-consignment-notes-qr-code-available/",
-# "https://www.theborneopost.com/2020/07/08/poslaju-customers-urged-to-bear-with-longer-waiting-time/") 
+P2.posLaju("https://www.thestar.com.my/business/business-news/2021/02/22/pos-malaysia-records-rm233b-revenue-in-fy20",
+"https://soyacincau.com/2020/08/15/pos-malaysia-e-consignment-notes-qr-code-available/",
+"https://www.theborneopost.com/2020/07/08/poslaju-customers-urged-to-bear-with-longer-waiting-time/") 
