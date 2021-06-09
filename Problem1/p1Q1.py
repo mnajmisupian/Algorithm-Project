b088da8cc2c280ca5b0a8e5200fe512e6b858b77
Aliff Aiman <aliffaimanridzuan@gmail.com>
2021-06-10 01:28:33 +0800

    fixed and cleaned problem 1 code

--- Problem1/p1Q1.py
+++ Problem1/p1Q1.py
@@ -6,33 +6,36 @@ from PIL import Image
 from io import BytesIO
 import matplotlib.pyplot as plt
 
-apikey = ''  
-base = f'https://maps.googleapis.com/maps/api/staticmap?key={apikey}&size=640x640&scale=2&'
+apikey = "AIzaSyAyPniwOWO5YfoLglfEovG02WngME4DE-I"
+base = (
+    f"https://maps.googleapis.com/maps/api/staticmap?key={apikey}&size=640x640&scale=2&"
+)
 
-def showImageFromUrl(url, name='map_points'):
+
+def showImageFromUrl(url, name="map_points"):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    plt.figure(figsize=(10,10))
-    plt.axis('off')
+    plt.figure(figsize=(10, 10))
+    plt.axis("off")
     plt.imshow(img)
-    plt.savefig(f'{name}.png')
+    plt.savefig(f"{name}.png")
     plt.show()
 
 
-cityLE=(3.0319924887507144,101.37344116244806)
-posLaju=(3.112924170027219,101.63982650389863)
-gdex=(3.265154613796736,101.68024844550233 )
-jnt=(3.265154613796736,101.68024844550233 )
-dhl=(3.2127230893650065,101.57467295692778)
-coordinates = [cityLE,posLaju,gdex,jnt,dhl]
-labels = ['C', 'P', 'G', 'J', 'D']
-colors = ['red','green','blue','purple','orange']
+cityLE = (3.0319924887507144, 101.37344116244806)
+posLaju = (3.112924170027219, 101.63982650389863)
+gdex = (3.265154613796736, 101.68024844550233)
+jnt = (3.265154613796736, 101.68024844550233)
+dhl = (3.2127230893650065, 101.57467295692778)
+coordinates = [cityLE, posLaju, gdex, jnt, dhl]
+labels = ["C", "P", "G", "J", "D"]
+colors = ["red", "green", "blue", "purple", "orange"]
 
 # Forming request, documentaion is at https://developers.google.com/maps/documentation/maps-static/start
 requestUrl = base
 
 for i in range(len(coordinates)):
-    requestUrl += f'&markers=color:{colors[i]}%7Clabel:{labels[i]}%7C{coordinates[i][0]},{coordinates[i][1]}'
+    requestUrl += f"&markers=color:{colors[i]}%7Clabel:{labels[i]}%7C{coordinates[i][0]},{coordinates[i][1]}"
 
 # Display the image from requestUrl
 showImageFromUrl(requestUrl)
--- Problem1/p1Q3.py
+++ Problem1/p1Q3.py
@@ -1,27 +1,11 @@
 import googlemaps
 import array
-apikey = ''
-gmapclient=googlemaps.Client(key=apikey)
 
-cityLE= (3.0319924887507144,101.37344116244806)
-posLaju=(3.112924170027219,101.63982650389863)
-gdex=(3.265154613796736,101.68024844550233 )
-jnt=(3.265154613796736,101.68024844550233 )
-dhl=(3.2127230893650065, 101.57467295692778)
-
-#customer1
-Rawang=(3.3615395462207878, 101.56318183511695)
-BukitJelutong= (3.1000170516638885, 101.53071480907951)
-#customer2
-SubangJaya=(3.049398375759954, 101.58546611160301)
-PuncakAlam=(3.227994355250716, 101.42730357605375)
-#customer3
-Ampang=(3.141855957281073, 101.76158583424586)
-Cyberjaya=(2.9188704151716256, 101.65251821655471)
+arrdistDic = {}
 
 
 def partition(arr, low, high):
-    i = (low - 1)  # index of smaller element
+    i = low - 1  # index of smaller element
     pivot = arr[high]  # pivot
 
     for j in range(low, high):
@@ -34,7 +18,7 @@ def partition(arr, low, high):
             arr[i], arr[j] = arr[j], arr[i]
 
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return (i + 1)
+    return i + 1
 
 
 def quickSort(arr, low, high):
@@ -50,54 +34,80 @@ def quickSort(arr, low, high):
         quickSort(arr, low, pi - 1)
         quickSort(arr, pi + 1, high)
 
-courier=[cityLE,posLaju,gdex,jnt,dhl]
-courierName=['cityLE','posLaju','gdex','jnt','dhl']
-origin=[Rawang,SubangJaya,Ampang]
-originName=['Rawang','SubangJaya','Ampang']
-destination=[BukitJelutong,PuncakAlam,Cyberjaya]
-destinationName=['BukitJelutong','PuncakAlam','Cyberjaya']
-distance=[]
+
+def main():
+    global arrdistDic
+    apikey = "AIzaSyAyPniwOWO5YfoLglfEovG02WngME4DE-I"
+    gmapclient = googlemaps.Client(key=apikey)
+
+    cityLE = (3.0319924887507144, 101.37344116244806)
+    posLaju = (3.112924170027219, 101.63982650389863)
+    gdex = (3.265154613796736, 101.68024844550233)
+    jnt = (2.9441205329488325, 101.7901521759029)
+    dhl = (3.2127230893650065, 101.57467295692778)
+
+    # customer1
+    Rawang = (3.3615395462207878, 101.56318183511695)
+    BukitJelutong = (3.1000170516638885, 101.53071480907951)
+    # customer2
+    SubangJaya = (3.049398375759954, 101.58546611160301)
+    PuncakAlam = (3.227994355250716, 101.42730357605375)
+    # customer3
+    Ampang = (3.141855957281073, 101.76158583424586)
+    Cyberjaya = (2.9188704151716256, 101.65251821655471)
+
+    courier = [cityLE, posLaju, gdex, jnt, dhl]
+    courierName = ["cityLE", "posLaju", "gdex", "jnt", "dhl"]
+    origin = [Rawang, SubangJaya, Ampang]
+    originName = ["Rawang", "SubangJaya", "Ampang"]
+    destination = [BukitJelutong, PuncakAlam, Cyberjaya]
+    destinationName = ["BukitJelutong", "PuncakAlam", "Cyberjaya"]
+    distance = []
+
+    j = 0
+    while j != 3:
+        print(f"customer {j+1}")
+        arrdistDic[j + 1] = {}
+        i = 0
+        while i != 5:
+            hub = courier[i]
+            name = courierName[i]
+            originPoint = origin[j]
+            destinationPoint = destination[j]
+            d_goog = gmapclient.distance_matrix(originPoint, hub, mode="driving")
+            d_goog2 = gmapclient.distance_matrix(hub, destinationPoint, mode="driving")
+            new_d = d_goog["rows"][0]["elements"][0]["distance"]["value"]
+            new_d2 = d_goog2["rows"][0]["elements"][0]["distance"]["value"]
+            print(
+                f"Driving distance from {originName[j]} -> {name} -> {destinationName[j]} :"
+            )
+            distance.append(new_d + new_d2)
+            arrdistDic[j + 1][courierName[i]] = new_d + new_d2
+            print((distance[i] / 1000), "km")
+            i = i + 1
+
+        print(distance)
+        print(arrdistDic)
+        sortedDistance = distance.copy()
+        # sortedDistance.sort()
+        n = len(sortedDistance)
+
+        quickSort(sortedDistance, 0, n - 1)
+        print(sortedDistance)
+        index = distance.index(sortedDistance[0])
+        print(distance.index(sortedDistance[0]))
+        shortestDistance = distance[index]
+        globaldistance = sortedDistance
+        print(
+            f"The shortest distance is using {courierName[index]} with distance of {shortestDistance/ 1000} km"
+        )
+        print()
+        distance = []
+        j = j + 1
 
 
-j=0
-while(j!=3):
-    print(f'customer {j+1}')
-    i = 0
-    while (i != 5):
-        hub = courier[i]
-        name = courierName[i]
-        originPoint = origin[j]
-        destinationPoint = destination[j]
-        d_goog = gmapclient.distance_matrix(originPoint, hub, mode='driving')
-        d_goog2 = gmapclient.distance_matrix(hub, destinationPoint, mode='driving')
-        new_d = d_goog['rows'][0]['elements'][0]['distance']['value']
-        new_d2 = d_goog2['rows'][0]['elements'][0]['distance']['value']
-        print(f'Driving distance from {originName[j]} -> {name} ->{destinationName[j]} :')
-        distance.append(new_d + new_d2)
-        print((distance[i] / 1000), 'km')
-        i = i + 1
-
-    print(distance)
-    sortedDistance = distance.copy()
-    # sortedDistance.sort()
-    n=len(sortedDistance)
-
-    quickSort(sortedDistance,0,n-1)
-    print(sortedDistance)
-    index = distance.index(sortedDistance[0])
-    print(distance.index(sortedDistance[0]))
-    shortestDistance=distance[index]
-    print(f'The shortest distance is using {courierName[index]} with distance of {shortestDistance/ 1000} km')
-    print()
-    distance=[]
-    j=j+1
-
-
-
-
-
-
-
+def gotop3():
+    return arrdistDic
 
 
 # i=0
@@ -122,10 +132,3 @@ while(j!=3):
 # index=distance.index(sortedDistance[0])
 # print(distance.index(sortedDistance[0]))
 # print(f'The shortest distance is using {courierName[index]} with distance of {distance[index]/1000} km' )
-
-
-
-
-
-
-
